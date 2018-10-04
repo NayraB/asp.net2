f7b697a321e7a14f6b9b129c374ead5a5c06f5a0
Nayra <nayramylena@hotmail.com>
2018-10-04 20:42:40 -0300

    alteração

---
+++ WebApplication2/Pages/Usuarios/Cadastrar.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Data;
+
+namespace WebApplication2.Pages.Usuarios
+{
+    public class CadastrarModel : PageModel
+    {
+        [BindProperty]
+        public Usuario usuario { get; set; }
+        public void OnGet(string nome, string senha)
+        {
+            if (usuario == null)
+            {
+                usuario = new Usuario();
+            }
+            usuario.Nome = nome;
+            usuario.Senha = senha;
+        }
+    }
+}
