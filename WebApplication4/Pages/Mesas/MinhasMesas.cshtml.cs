378d621614e6d700c4826cd7235d239a79e6c300
Nayra <nayramylena@hotmail.com>
2018-11-20 19:50:37 -0200

    Adicionando a lista de mesas

--- WebApplication4/Pages/Mesas/MinhasMesas.cshtml.cs
+++ WebApplication4/Pages/Mesas/MinhasMesas.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApplication4.Business;
+using WebApplication4.Data;
 
 namespace WebApplication4.Pages.Mesas
 {
@@ -20,8 +21,20 @@ namespace WebApplication4.Pages.Mesas
         [BindProperty]
         [Required(ErrorMessage ="O nome da mesa é obrigatório")]
         public string NomeMesa { get; set; }
+        public List<Mesa> mesas { get; set; }
+
         public void OnGet()
         {
+            // Pegando Id do usuário da session
+            int? usuarioId = // A interrogação depois do int significa que o int pode ser null
+                   HttpContext.Session.GetInt32("usuarioId");
+            if (!usuarioId.HasValue)
+            {
+                throw new Exception("Erro de permissão");
+            }
+
+            //O service é o que lê as mesas e depois estou atribuindo os valores para a variável mesas
+            mesas =_jogoService.ListarMesa(usuarioId ?? 0);
         }
         public void OnPost()
         {
